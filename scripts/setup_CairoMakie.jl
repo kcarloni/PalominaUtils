cc6fef232c2c233a3a2ec157a3380f033a252f8c
kcarloni <kcarloni@g.harvard.edu>
2025-08-13 23:31:16 -0400

    add arrow plot convenience methods

--- scripts/setup_CairoMakie.jl
+++ scripts/setup_CairoMakie.jl
@@ -20,3 +20,24 @@ import CairoMakie.Makie.UnicodeFun: to_superscript
 const inch = 96
 const pt = 4/3
 
+
+function CairoMakie.arrows2d!(ax, 
+    x::Real, y::Real, u::Real, v::Real; 
+    text=nothing, arrow_kwargs=NamedTuple(), text_kwargs=NamedTuple()
+    )
+
+    arrows2d!( ax, [x], [y], [u], [v]; arrow_kwargs... )
+    if !isnothing( text )
+        lengthscale = hasproperty(arrow_kwargs, :lengthscale) ? arrow_kwargs.lengthscale : 1
+        text!( ax, x+u*lengthscale, y+v*lengthscale; text, text_kwargs... )
+    end
+end
+
+function CairoMakie.arrows3d!(ax, 
+    x::Real, y::Real, z::Real, 
+    u::Real, v::Real, w::Real; kwargs...) 
+    
+    arrows3d!( ax, 
+        [x], [y], [z], 
+        [u], [v], [w]; kwargs... )
+end
